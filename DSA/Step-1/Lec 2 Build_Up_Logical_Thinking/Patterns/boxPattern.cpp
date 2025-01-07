05c89f25c6f8063e8eb1f020ecc905e926bd2607
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-01-07 22:31:13 +0530

    try

--- DSA/Step-1/Lec 2 Build_Up_Logical_Thinking/Patterns/boxPattern.cpp
+++ DSA/Step-1/Lec 2 Build_Up_Logical_Thinking/Patterns/boxPattern.cpp
@@ -70,18 +70,44 @@ void pattern6(int n)
 }
 void pattern7(int n)
 {
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j < ((2*n) - 1); j++)
+        for (int j = 0; j < n-i-1; j++)
         {
-            if(j >= n-i-1 && j<= n+i-1)
-            {
-                cout << " * ";
-            }
-            
+            cout<<"   ";
         }
+        for(int j = 0; j< 2*i+1;j++){
+            cout<<" * ";
+        }
+        for (int l = 0; l < n-i-1 ; i++)
+        {
+            cout<<"   ";
+        }
+        
         cout << endl;
     }
+}
+void pattern8(int n){
+    for (int i = n-1 ; i >= 0; i--)
+    {
+        for (int j = n-1; j < n-i-1 ; j--)
+        {
+            cout<<"   ";
+        }
+
+        for (int k = n-1; k >=0; k--)
+        {
+            cout<<" * ";
+        }
+        
+        for (int l = n-1; l > n-i-1; l--)
+        {
+           cout<<"   ";
+        }
+        cout<<endl;
+        
+    }
+    
 }
 int main()
 {
@@ -101,7 +127,8 @@ int main()
         // pattern4(n);
         // pattern5(n);
         // pattern6(n);
-        pattern7(n);
+        // pattern7(n);
+        pattern8(n);
     }
 
     return 0;
