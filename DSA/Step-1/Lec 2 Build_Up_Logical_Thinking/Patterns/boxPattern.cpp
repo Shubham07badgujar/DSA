3c7e13552bc163375891a7d73f5dd04b48ee95c0
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-01-07 20:03:54 +0530

    pattern 6

--- DSA/Step-1/Lec 2 Build_Up_Logical_Thinking/Patterns/boxPattern.cpp
+++ DSA/Step-1/Lec 2 Build_Up_Logical_Thinking/Patterns/boxPattern.cpp
@@ -1,31 +1,108 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void pattern1(int n){
+void pattern1(int n)
+{
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n;j++)
+        for (int j = 0; j < n; j++)
         {
-            cout<<" * ";
- 
+            cout << " * ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
-int main(){
-    int n,m;
-    cout<<"Enter the how many cases you have to run ";
-    cin>>m;
-    
+void pattern2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern3(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << j + 1 << "\t";
+        }
+        cout << endl;
+    }
+}
+void pattern4(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << i + 1;
+        }
+        cout << endl;
+    }
+}
+void pattern5(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+void pattern6(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << j+1;
+        }
+        cout << endl;
+    }
+}
+void pattern7(int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < ((2*n) - 1); j++)
+        {
+            if(j >= n-i-1 && j<= n+i-1)
+            {
+                cout << " * ";
+            }
+            
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n, m;
+    cout << "Enter the how many cases you have to run ";
+    cin >> m;
+
     // pattern1(n);
     for (int i = 0; i < m; i++)
     {
-        cout<<"Enter Number For Case"<<i<<endl;
-        int n;
-        cin>>n;
-        pattern1(n);
+        cout << "Enter Number For Case" << i << endl;
+        int n = 3;
+        cin >> n;
+        // pattern1(n);
+        // pattern2(n);
+        // pattern3(n);
+        // pattern4(n);
+        // pattern5(n);
+        // pattern6(n);
+        pattern7(n);
     }
-    
 
-    return 0;    
+    return 0;
 }
